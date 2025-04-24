dac35499abe38c3f937ade7a17274c9b707b031b
mt9ing <mt9ing@gmail.com>
2025-04-24 20:45:30 +0200

    using .env to get host and port data

--- server.ts
+++ server.ts
@@ -1,7 +1,9 @@
 import application from "./src/application";
 import { Response, Request } from "express";
+import "dotenv/config";
 
 const PORT: number = Number(process.env.SERVER_PORT) || 3000;
+const HOST: string = process.env.SERVER_HOST || "localhost";
 
 application.get("/status", (request: Request, response: Response) => {
   response
@@ -9,8 +11,8 @@ application.get("/status", (request: Request, response: Response) => {
     .json({ status: "Working!", message: "No Errors Currently!" });
 });
 
-application.listen(PORT, () => {
+application.listen(PORT, () =>
   console.log(
-    `Server Running on: http://localhost:${PORT} - "You can check server status by visiting {/status}".`
-  );
-});
+    `Server Running on: http://${HOST}:${PORT} - "You can check server status by visiting {/status}".`
+  )
+);

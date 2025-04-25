62a6f99d348b5fa990d9674fd963c7afa4ab20b5
mt9ing <mt9ing@gmail.com>
2025-04-25 17:55:13 +0300

    added ejs configure, some comments,  and contribution route

--- src/application.ts
+++ src/application.ts
@@ -1,9 +1,19 @@
 import express, { Express } from "express";
+import path from "path";
+import contributionRouter from "./routes/contribution.route";
 
-// Configurations
+// Application Configurations
 const application: Express = express();
+
+// Express Configurations
+application.set("view engine", "ejs");
+application.set("views", path.join(__dirname, "views"));
 
 // Middlewares
 application.use(express.json());
 
+// Routes
+application.use("/", contributionRouter);
+
+// Export Express Application To The Server File
 export default application;
